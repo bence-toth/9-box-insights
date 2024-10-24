19395f47e0c9752e18d606abb8eb1d2c4927a057
Bence A. Toth <tothab@gmail.com>
2024-10-24 17:02:36 +0200

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,13 @@
-const topRightElbow = [6, 8, 9];
-const bottomLeftElbow = [1, 2, 4];
-const mainDiagonal = [3, 5, 7];
+const boxThresholds: { box: number; min?: number; max?: number }[] = [];
 
-const boxThresholds: { box: number; min?: number; max?: number }[] = [
-  { box: 9, max: 0.2 },
-];
+const topRightElbow = [6, 8, 9];
+// const bottomLeftElbow = [1, 2, 4];
+// const mainDiagonal = [3, 5, 7];
 
 const boxGroupThresholds: {
   boxes: number[];
   min?: number;
   max?: number;
-}[] = [
-  { boxes: topRightElbow, max: 0.35 },
-  { boxes: bottomLeftElbow, min: 0 },
-  { boxes: mainDiagonal, min: 0 },
-];
+}[] = [{ boxes: topRightElbow, max: 0.33 }];
 
 export { boxThresholds, boxGroupThresholds };
